ef641fd9e648232d501a6ce4ec9d368fedeb7333
zzhou370 <zzhou370@gmail.com>
2026-03-22 23:12:54 +0800

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+# pages/urls.py
+from django.urls import path
+from .views import home_page_view, about_page_view # new
+urlpatterns = [
+ path("about/", about_page_view), # new
+ path("", home_page_view),
+]
